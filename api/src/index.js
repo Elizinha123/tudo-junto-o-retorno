6c6eb8904c63c3b02b388b3c1c2fd8baae2f4e69
eliza <elizaxavier2@gmail.com>
2022-05-31 16:56:08 -0300

    meu deus meu senhor me ajuda pfvr

---
+++ api/src/index.js
@@ -0,0 +1,15 @@
+import 'dotenv/config'
+
+import usuarioController from './controller/usuarioController.js';
+import filmeController from './controller/filmeController.js';
+
+import express from 'express'
+import cors from 'cors'
+
+server.use(cors());
+server.use(express.json());
+
+server.use(usuarioController);
+server.use(filmeController);
+
+server.listen(process.env.PORT, () => console.log(`API CONECTADO NA PORTA ${process.env.PORT}`)); 
